e387ad3aaa893560718a8fa111bd8b8404a13da8
lil bey <christyan.henrique@gmail.com>
2026-09-26 12:51:02 -0300

    Update module base URL for sangzap app

--- modules/phone/apps/sangzap/shell.js
+++ modules/phone/apps/sangzap/shell.js
@@ -9,7 +9,7 @@
 
     const APP_ID = 'sangzap';
     const APP_VERSION = '0.6.0';
-    const DEFAULT_MODULE_BASE = 'https://cdn.jsdelivr.net/gh/zBeyond5/Liveblock@main/modules/phone';
+    const DEFAULT_MODULE_BASE = 'https://raw.githubusercontent.com/zBeyond5/Liveblock/main/modules/phone';
     const DEFAULT_APP_BG = '#0e1621';
 
     const MODULE_BASE = (ctx.moduleBase || DEFAULT_MODULE_BASE).replace(/\/+$/, '');
